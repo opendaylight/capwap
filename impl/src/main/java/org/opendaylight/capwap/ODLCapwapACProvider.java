eda33405c64e3fb5c5e7e3847cb5e8b6b5ea32a2
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-08-07 14:11:02 -0400

    Fix license header violations
    Change-Id: I6a243ceb006ca106c9fd28d9979ffd70fe278ec6
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- impl/src/main/java/org/opendaylight/capwap/ODLCapwapACProvider.java
+++ impl/src/main/java/org/opendaylight/capwap/ODLCapwapACProvider.java
@@ -1,8 +1,11 @@
 /*
+ * Copyright (c) 2015 Navin Agrawal and others.  All rights reserved.
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
+
 package org.opendaylight.capwap;
 
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
