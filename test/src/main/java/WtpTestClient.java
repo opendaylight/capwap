5ede862d2101773a26a113e08175bacffddcb324
Alexis de Talhouët <adetalhouet@inocybe.com>
2016-05-05 19:24:03 -0400

    Use odlparent for aggregator
    And fix some copyright issues
    
    Change-Id: I4db0036f792e1cacd13a6e691239a23e6a5231ea
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

--- test/src/main/java/WtpTestClient.java
+++ test/src/main/java/WtpTestClient.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2015 Mahesh Govind and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import org.opendaylight.capwap.ODLCapwapConsts;
